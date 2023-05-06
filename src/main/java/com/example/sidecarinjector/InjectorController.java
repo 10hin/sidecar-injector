600fa3045b30216dba160cb4b2021a8669e8f3a2
10hin <10hin@outlook.jp>
2023-05-07 01:08:44 +0900

    use Jackson to manipulate JSON

--- src/main/java/com/example/sidecarinjector/InjectorController.java
+++ src/main/java/com/example/sidecarinjector/InjectorController.java
@@ -1,18 +1,20 @@
 package com.example.sidecarinjector;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import io.kubernetes.client.admissionreview.models.AdmissionResponse;
 import io.kubernetes.client.admissionreview.models.AdmissionReview;
 import io.kubernetes.client.admissionreview.models.GroupVersionResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.nio.charset.StandardCharsets;
 
 
 @RestController
@@ -30,6 +32,19 @@ public class InjectorController {
 
     private final static String ADMISSIONREVIEW_API_VERSION = "admission.k8s.io/v1";
     private final static String ADMISSIONREVIEW_KIND = "AdmissionReview";
+
+    private final ObjectMapper objectMapper;
+    private final JsonNodeFactory jsonNodeFactory;
+
+    @Autowired
+    public InjectorController(
+            final ObjectMapper objectMapper
+    ) {
+
+        this.objectMapper = objectMapper;
+        this.jsonNodeFactory = this.objectMapper.getNodeFactory();
+
+    }
 
     @PostMapping("")
     public Mono<ResponseEntity<AdmissionReview>> mutate(
@@ -72,8 +87,7 @@ public class InjectorController {
                     response.setUid(uid);
                     response.setAllowed(true);
                     response.setPatchType("JSONPatch");
-                    final var patch = "[{\"op\":\"add\",\"path\":\"/spec/containers/-\",\"value\":{\"name\":\"sidecar-nginx\",\"image\":\"nginx:1.21-alpine\",\"imagePullPolicy\":\"IfNotPresent\"}}]";
-                    response.setPatch(patch.getBytes(StandardCharsets.UTF_8));
+                    response.setPatch(createPatch());
 
                     final var reviewResponse = new AdmissionReview()
                             .apiVersion(ADMISSIONREVIEW_API_VERSION)
@@ -92,6 +106,27 @@ public class InjectorController {
     private static class RequestProblemException extends RuntimeException {
         public RequestProblemException(Throwable cause) {
             super(cause);
+        }
+    }
+
+    private byte[] createPatch() {
+        final var containerEntry = this.jsonNodeFactory.objectNode()
+                .put("name", "sidecar-nginx")
+                .put("image", "nginx:1.21-alpine")
+                .put("imagePullPolicy", "IfNotPresent");
+
+        final var addContainerOperation = this.jsonNodeFactory.objectNode()
+                .put("op", "add")
+                .put("path", "/spec/containers/-")
+                .set("value", containerEntry);
+
+        final var patchArray = this.jsonNodeFactory.arrayNode(1)
+                .add(addContainerOperation);
+
+        try {
+            return this.objectMapper.writeValueAsBytes(patchArray);
+        } catch (JsonProcessingException e) {
+            throw new InternalError("failed to serialize (format) to JSON", e);
         }
     }
 
